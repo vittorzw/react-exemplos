3e7eae09189eb7349128f201def710870c64fcd2
vitor <hugov3141v@gmail.com>
2024-04-18 14:23:40 -0300

    .

--- todolist/TodoList/src/App.jsx
+++ todolist/TodoList/src/App.jsx
@@ -1,35 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      Text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {id: 2,text: "Ir para a academia", categry: "Pessoal", isCmpleted: false},
+    {
+      id:3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <h1>Lsita de Tarefas</h1>
+      <div className='todo-list'>
+        {todos.map((todo)=>(
+          <div className='todo'>
+            <div className='content'>
+            <p>{todo.text}</p>
+            <p className='category'>({todo.category})</p>
+    
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+          <button>Completar</button>
+          <button>X</button>
+          </div>
+          </div>
+        ))}
+         </div>
+          </div>
   )
+
+
 }
 
 export default App
---
+++ todolist/components/todo.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Todo = () => {
+    return (
+          <div className='todo'>
+            <div className='content'>
+            <p>{todo.text}</p>
+            <p className='category'>({todo.category})</p>
+    
+      </div>
+    </div>
+          <button>Completar</button>
+          <button>X</button>
+          </div>
+          </div>
+    )
+}
+
+export default Todo
